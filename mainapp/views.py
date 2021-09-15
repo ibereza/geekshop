2026d97e368a4f8d50bc79cf4bc802db9196e9e6
Igor Bereza <igor.bereza@gmail.com>
2021-09-15 18:21:41 +0300

    added products.json, edited mainapp.views.products with open JSON

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import json
 
 from django.shortcuts import render
 
@@ -8,52 +9,11 @@ def index(request):
 
 
 def products(request):
+    with open('mainapp/fixtu/products.json', encoding='UTF-8') as f:
+        product_base = json.load(f)
     content = {
         'title': 'GeekShop - Каталог',
         'datetime': datetime.now(),
-        'cards': [
-            {
-                'img': 'vendor/img/products/Adidas-hoodie.png',
-                'title': 'Худи черного цвета с монограммами adidas Originals',
-                'price': '6 090,00',
-                'text': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
-                'in_basket': False
-            },
-            {
-                'img': 'vendor/img/products/Blue-jacket-The-North-Face.png',
-                'title': 'Синяя куртка The North Face',
-                'price': '23 725,00',
-                'text': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.',
-                'in_basket': True
-            },
-            {
-                'img': 'vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png',
-                'title': 'Коричневый спортивный oversized-топ ASOS DESIGN',
-                'price': '3 390,00',
-                'text': 'Материал с плюшевой текстурой. Удобный и мягкий.',
-                'in_basket': False
-            },
-            {
-                'img': 'vendor/img/products/Black-Nike-Heritage-backpack.png',
-                'title': 'Черный рюкзак Nike Heritage',
-                'price': '2 340,00',
-                'text': 'Плотная ткань. Легкий материал.',
-                'in_basket': False
-            },
-            {
-                'img': 'vendor/img/products/Black-Dr-Martens-shoes.png',
-                'title': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex',
-                'price': '13 590,00',
-                'text': 'Гладкий кожаный верх. Натуральный материал.',
-                'in_basket': False
-            },
-            {
-                'img': 'vendor/img/products/Dark-blue-wide-leg-ASOs-DESIGN-trousers.png',
-                'title': 'Темно-синие широкие строгие брюки ASOS DESIGN',
-                'price': '2 890,00',
-                'text': 'Легкая эластичная ткань сирсакер Фактурная ткань.',
-                'in_basket': False
-            }
-        ]
+        'cards': product_base
     }
     return render(request, 'mainapp/products.html', content)
