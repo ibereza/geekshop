5e0b5a5a424d305453ba399cb68799e618530129
Igor Bereza <igor.bereza@gmail.com>
2021-09-18 14:33:22 +0300

    cosmetic changes

--- mainapp/models.py
+++ mainapp/models.py
@@ -2,17 +2,20 @@ from django.db import models
 
 
 class ProductCategory(models.Model):
-    name = models.CharField(verbose_name='category_name', max_length=64, unique=True)
-    description = models.TextField(verbose_name='category_description', blank=True)
+    name = models.CharField(verbose_name='название категории', max_length=64, unique=True)
+    description = models.TextField(verbose_name='описание категории', blank=True)
 
     def __str__(self):
         return self.name
 
 
 class Products(models.Model):
-    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
-    name = models.CharField(verbose_name='product_name', max_length=128)
-    image = models.ImageField(upload_to='product_images', blank=True)
-    description = models.TextField(verbose_name='product_description', blank=True)
-    price = models.DecimalField(verbose_name='product_price', max_digits=8, decimal_places=2, default=0)
-    quantity = models.PositiveIntegerField(verbose_name='quantity', default=0)
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE, verbose_name='категория')
+    name = models.CharField(verbose_name='название', max_length=128)
+    image = models.ImageField(verbose_name='фото', upload_to='products_images', blank=True)
+    description = models.TextField(verbose_name='описание', blank=True)
+    price = models.DecimalField(verbose_name='цена', max_digits=8, decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField(verbose_name='количество', default=0)
+
+    def __str__(self):
+        return f'{self.name} | {self.category}'
