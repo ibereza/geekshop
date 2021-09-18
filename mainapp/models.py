26eab84316d951e3a8ca8834235902a21c9f481f
Igor Bereza <igor.bereza@gmail.com>
2021-09-18 11:45:42 +0300

    created models ProductCategory and Products. made migrates. added media directory and correspond settings of media.

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(verbose_name='category_name', max_length=64, unique=True)
+    description = models.TextField(verbose_name='category_description', blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Products(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    name = models.CharField(verbose_name='product_name', max_length=128)
+    image = models.ImageField(upload_to='product_images', blank=True)
+    description = models.TextField(verbose_name='product_description', blank=True)
+    price = models.DecimalField(verbose_name='product_price', max_digits=8, decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField(verbose_name='quantity', default=0)
