28a40028ac12fe7126086177f2229ff15ec85184
Igor Bereza <igor.bereza@gmail.com>
2021-09-18 21:13:35 +0300

    edited products.html: src="{{ product.image.url }}"

--- mainapp/views.py
+++ mainapp/views.py
@@ -12,8 +12,6 @@ def index(request):
 def products(request):
     category_content = ProductCategory.objects.all()
     products_content = Products.objects.all()
-    # with open('mainapp/fixtu/products.json', encoding='UTF-8') as f:
-    #     products_base = json.load(f)
     content = {
         'title': 'GeekShop - Каталог',
         'datetime': datetime.now(),
