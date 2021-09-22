92549f4dca8aab5004fe27f966a1b441b7289b46
Igor Bereza <igor.bereza@gmail.com>
2021-09-22 18:42:55 +0300

    created model User

--- users/models.py
+++ users/models.py
@@ -1,2 +1,7 @@
 from django.db import models
 
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    image = models.ImageField(verbose_name='аватар', upload_to='user_image', blank=True)
