45f56688ee06339b8c8fce4723a976ee5fea9af0
Igor Bereza <igor.bereza@gmail.com>
2021-09-25 22:13:09 +0300

    added logout in users:views.py and users:urls.py edited main:base.html

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,5 @@ app_name = 'users'
 urlpatterns = [
     path('login/', users.login, name='login'),
     path('register/', users.register, name='register'),
+    path('logout/', users.logout, name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -42,3 +42,8 @@ def register(request):
         'form': form
     }
     return render(request, 'users/register.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
