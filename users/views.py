60329f2b9596266996c41af736a97760e0a7c8ac
Igor Bereza <igor.bereza@gmail.com>
2021-09-25 20:04:25 +0300

    added UserLoginForm in forms.py

--- users/views.py
+++ users/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
+
+from users.forms import UserLoginForm
+
 
 def login(request):
-     context = {
-         'title': 'Geekshop - Авторизация'
-     }
-     return render(request, 'users/login.html', context)
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+        else:
+            print(form.errors)
+    else:
+        form = UserLoginForm()
+    context = {
+        'title': 'Geekshop - Авторизация',
+        'form': form
+    }
+
+    return render(request, 'users/login.html', context)
 
 
 def register(request):
