ec23181ec43bb1227bcdf7bdccd806083b16c8f4
Igor Bereza <igor.bereza@gmail.com>
2021-09-26 16:46:58 +0300

    added clean_image in UserProfileForm

--- users/forms.py
+++ users/forms.py
@@ -51,3 +51,9 @@ class UserProfileForm(UserChangeForm):
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control py-4'
         self.fields['image'].widget.attrs['class'] = 'custom-file-input'
+
+    def clean_image(self):
+        data = self.cleaned_data['image']
+        if data.size > 1024:
+            raise forms.ValidationError('Файл слишком большой')
+        return data
