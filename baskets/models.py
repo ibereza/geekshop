d877671610404170192cc3c0e6236819ed533b24
Igor Bereza <igor.bereza@gmail.com>
2021-09-27 06:52:15 +0300

    added sum in baskets:models.py

--- baskets/models.py
+++ baskets/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from users.models import User
 from mainapp.models import Products
 
+
 class Basket(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Products, on_delete=models.CASCADE)
@@ -10,6 +11,8 @@ class Basket(models.Model):
     created_timestamp = models.DateTimeField(auto_now_add=True)
     update_timestamp = models.DateTimeField(auto_now=True)
 
-
     def __str__(self):
         return f'Корзина для {self.user.username} | Продукт {self.product.name}'
+
+    def sum(self):
+        return self.quantity * self.product.price
