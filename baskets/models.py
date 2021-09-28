ab69a18a1f90e87469f3f054c6e0df83972f3a53
Igor Bereza <igor.bereza@gmail.com>
2021-09-28 10:44:45 +0300

    added calculation of the total quantity and value of added products in basket:models

--- baskets/models.py
+++ baskets/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+# from django.db.models import Sum
 
 from users.models import User
 from mainapp.models import Products
@@ -14,5 +15,23 @@ class Basket(models.Model):
     def __str__(self):
         return f'Корзина для {self.user.username} | Продукт {self.product.name}'
 
+    @property
     def sum(self):
+        """return sum quantity for user"""
         return self.quantity * self.product.price
+
+    @property
+    def total_quantity(self):
+        """return total quantity for user"""
+        items = Basket.objects.filter(user=self.user)
+        total_quantity = sum(list(map(lambda x: x.quantity, items)))
+        # total_quantity = Basket.objects.filter(user=self.user).aggregate(Sum('quantity'))['quantity__sum']
+        return total_quantity
+
+    @property
+    def total_cost(self):
+        """return total cost for user"""
+        items = Basket.objects.filter(user=self.user)
+        total_cost = sum(list(map(lambda x: x.sum, items)))
+        # total_cost = sum(list(map(lambda x: x.quantity * x.product.price, _items)))
+        return total_cost
