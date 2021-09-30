f0cb16b60be6865eb8313356ce12a3316478bee5
Igor Bereza <igor.bereza@gmail.com>
2021-09-30 11:22:25 +0300

    added AJAX in baskets

--- baskets/urls.py
+++ baskets/urls.py
@@ -5,6 +5,7 @@ import baskets.views as baskets
 app_name = 'baskets'
 
 urlpatterns = [
-    path('add/<int:product_id>', baskets.basket_add, name='basket'),
-    path('remove/<int:product_id>', baskets.basket_remove, name='basket_remove'),
+    path('add/<int:product_id>/', baskets.basket_add, name='basket'),
+    path('remove/<int:product_id>/', baskets.basket_remove, name='basket_remove'),
+    path('edit/<int:basket_id>/<int:quantity>/', baskets.basket_edit, name='basket_edit'),
 ]
