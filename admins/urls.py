6bfb7f2d408d2547fb38e9a4dce9bcb342c6b959
Igor Bereza <igor.bereza@gmail.com>
2021-10-02 14:35:04 +0300

    created base classes for admin users in admins:views created paths in admins:urls

---
+++ admins/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+import admins.views as admins
+
+app_name = 'admins'
+
+urlpatterns = [
+    path('', admins.index, name='index'),
+    path('users/', admins.UserListView.as_view(), name='admins_user'),
+    path('users-create/', admins.UserCreateView.as_view(), name='admins_user_create'),
+    path('users-update/<int:pk>/', admins.UserUpdateView.as_view(), name='admins_user_update'),
+    path('users-delete/', admins.DeleteView.as_view(), name='admins_user_delete'),
+]
