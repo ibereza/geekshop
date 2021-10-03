a804f1dc253a421633b129fa64554a0780587bf1
Igor Bereza <igor.bereza@gmail.com>
2021-10-03 14:15:31 +0300

    edited UserListView in admins:views

--- admins/views.py
+++ admins/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
+from users.models import User
+
 
 def index(request):
     return render(request, 'admins/admin.html')
 
 
 class UserListView(ListView):
-    pass
+    model = User
+    template_name = 'admins/admin-users-read.html'
+    context_object_name = 'users'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(UserListView, self).get_context_data(**kwargs)
+        context['title'] = 'Админка | Пользователи'
+        return context
 
 
 class UserCreateView(CreateView):
