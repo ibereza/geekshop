636bd40ff6de8448c0aba1d9db993c0c10dd9214
Igor Bereza <igor.bereza@gmail.com>
2021-10-03 15:29:29 +0300

    edited UserCreateView in admins:views

--- admins/views.py
+++ admins/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
+from admins.forms import UserAdminRegisterForm
 from users.models import User
 
 
@@ -20,7 +22,15 @@ class UserListView(ListView):
 
 
 class UserCreateView(CreateView):
-    pass
+    model = User
+    template_name = 'admins/admin-users-create.html'
+    form_class = UserAdminRegisterForm
+    success_url = reverse_lazy('admins:admins_user')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(UserCreateView, self).get_context_data(**kwargs)
+        context['title'] = 'Админка | Регистрация'
+        return context
 
 
 class UserUpdateView(UpdateView):
