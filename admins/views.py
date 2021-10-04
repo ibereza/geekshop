7b88a8f0f628f63af9e40fc8ccceae063c0c6a8f
Igor Bereza <igor.bereza@gmail.com>
2021-10-04 07:40:13 +0300

    added protection for admins:views via function dispatch and added mixin.py with class CustomDispatchMixin

--- admins/views.py
+++ admins/views.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.decorators import user_passes_test
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from admins.forms import UserAdminRegisterForm, UserAdminProfileForm
+from geekshop.mixin import CustomDispatchMixin
 from users.models import User
 
 
@@ -11,7 +14,7 @@ def index(request):
     return render(request, 'admins/admin.html')
 
 
-class UserListView(ListView):
+class UserListView(ListView, CustomDispatchMixin):
     model = User
     template_name = 'admins/admin-users-read.html'
     context_object_name = 'users'
@@ -22,7 +25,7 @@ class UserListView(ListView):
         return context
 
 
-class UserCreateView(CreateView):
+class UserCreateView(CreateView, CustomDispatchMixin):
     model = User
     template_name = 'admins/admin-users-create.html'
     form_class = UserAdminRegisterForm
@@ -34,7 +37,7 @@ class UserCreateView(CreateView):
         return context
 
 
-class UserUpdateView(UpdateView):
+class UserUpdateView(UpdateView, CustomDispatchMixin):
     model = User
     template_name = 'admins/admin-users-update-delete.html'
     form_class = UserAdminProfileForm
@@ -50,6 +53,10 @@ class UserDeleteView(DeleteView):
     model = User
     template_name = 'admins/admin-users-update-delete.html'
     success_url = reverse_lazy('admins:admins_user')
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserDeleteView, self).dispatch(request, *args, **kwargs)
 
     def delete(self, request, *args, **kwargs):
         self.object = self.get_object()
