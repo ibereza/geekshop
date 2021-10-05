6b33eaca852baf6f580510de30a8284aa8e84a7f
Igor Bereza <igor.bereza@gmail.com>
2021-10-05 09:14:25 +0300

    edited mainapp:admin via class, added user basket to users:admin

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from .models import ProductCategory, Products
 
 admin.site.register(ProductCategory)
-admin.site.register(Products)
+# admin.site.register(Products)
+
+
+@admin.register(Products)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('name', 'image', 'description', ('price', 'quantity'), 'category')
+    readonly_fields = ('description',)
+    ordering = ('name', 'price')
+    search_fields = ('name',)
