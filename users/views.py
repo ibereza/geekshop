3713929dc22ea1f63c96560f13b7b0db721a0721
Igor Bereza <igor.bereza@gmail.com>
2021-10-09 15:21:09 +0300

    replaced functions with classes at users:views

--- users/views.py
+++ users/views.py
@@ -1,68 +1,57 @@
-from django.shortcuts import render, HttpResponseRedirect
-from django.contrib import auth, messages
-from django.urls import reverse
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import redirect, get_object_or_404
+from django.contrib import messages
+from django.urls import reverse_lazy
+from django.views.generic import FormView, UpdateView
 
 from baskets.models import Basket
+from geekshop.mixin import BaseClassContextMixin
 from users.forms import UserLoginForm, UserRegisterForm, UserProfileForm
-from django.contrib.auth.decorators import login_required
+from users.models import User
 
 
-def login(request):
-    if request.method == 'POST':
-        form = UserLoginForm(data=request.POST)
-        if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
-            user = auth.authenticate(username=username, password=password)
-            if user.is_active:
-                auth.login(request, user)
-                return HttpResponseRedirect(reverse('index'))
-    else:
-        form = UserLoginForm()
-    context = {
-        'title': 'Geekshop - Авторизация',
-        'form': form
-    }
-
-    return render(request, 'users/login.html', context)
+class LoginLoginView(LoginView, BaseClassContextMixin):
+    template_name = 'users/login.html'
+    form_class = UserLoginForm
+    title = 'Geekshop - Авторизация'
 
 
-def register(request):
-    if request.method == 'POST':
-        form = UserRegisterForm(data=request.POST)
+class RegisterListView(FormView, BaseClassContextMixin):
+    model = User
+    template_name = 'users/register.html'
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('users:login')
+    title = 'Geekshop - Регистрация'
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(data=request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, 'Вы успешно зарегистрировались!')
-            return HttpResponseRedirect(reverse('users:login'))
-    else:
-        form = UserRegisterForm()
-    context = {
-        'title': 'Geekshop - Регистрация',
-        'form': form
-    }
-    return render(request, 'users/register.html', context)
+        return redirect(self.success_url)
 
 
-@login_required
-def profile(request):
-    if request.method == 'POST':
-        form = UserProfileForm(data=request.POST,
-                               files=request.FILES,
-                               instance=request.user)  # instance - обновит данные, а не создаст новую запись
+class ProfileFormView(UpdateView, BaseClassContextMixin):
+    model = User
+    form_class = UserProfileForm
+    template_name = 'users/profile.html'
+    success_url = reverse_lazy('users:profile')
+    title = 'Geekshop - Профайл'
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(User, pk=self.request.user.pk)
+
+    def get_context_data(self, **kwargs):
+        context = super(ProfileFormView, self).get_context_data(**kwargs)
+        context['baskets'] = Basket.objects.filter(user=self.request.user)
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(data=request.POST, files=request.FILES, instance=self.get_object())
         if form.is_valid():
             form.save()
-            messages.success(request, "Ваш профиль был успешно обновлен")
-            return HttpResponseRedirect(reverse('users:profile'))
-        else:
-            messages.error(request, "Профиль не сохранён")
-    context = {
-        'title': 'Geekshop - Профайл',
-        'form': UserProfileForm(instance=request.user),  # instance - заполнит поля текущими значениями
-        'baskets': Basket.objects.filter(user=request.user),
-    }
-    return render(request, 'users/profile.html', context)
+        return redirect(self.success_url)
 
 
-def logout(request):
-    auth.logout(request)
-    return HttpResponseRedirect(reverse('index'))
+class Logout(LogoutView):
+    template_name = 'mainapp/index.html'
