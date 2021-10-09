fab284e5c5798a55e5638062d4defd0758472bc9
Igor Bereza <igor.bereza@gmail.com>
2021-10-09 13:19:00 +0300

    added pagination

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,4 +6,5 @@ app_name = 'mainapp'
 urlpatterns = [
     path('', mainapp.products, name='index'),
     path('category/<int:category_id>/', mainapp.products, name='category'),
+    path('page/<int:page_id>/', mainapp.products, name='page'),
 ]
